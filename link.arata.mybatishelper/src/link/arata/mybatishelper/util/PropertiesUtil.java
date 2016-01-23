0e7c5f45ed47a9a560a24c83bf63234f7bfc9130
webarata3 <webarata3@gmail.com>
2016-01-24 06:00:07 +0900

    ライセンスの変更

--- link.arata.mybatishelper/src/link/arata/mybatishelper/util/PropertiesUtil.java
+++ link.arata.mybatishelper/src/link/arata/mybatishelper/util/PropertiesUtil.java
@@ -1,17 +1,10 @@
-/*
- * Copyright 2015 Shinichi ARATA
+/**
+ * MyBatis Helper
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright (c) 2016 Shinichi ARATA
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
  */
 package link.arata.mybatishelper.util;
 
